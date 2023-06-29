947d0f6b7b595459a5578b8d1a158574fd31e370
“anjana <anjanananayakkara1999@gmail.com>
2023-06-29 23:38:19 +0530

    initial commit

---
+++ bro_code/src/Main.java
@@ -0,0 +1,78 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args){
+
+//        ----------1-----------
+//        System.out.print("hello world\n");
+//        System.out.println("hello anjana");
+
+//        ------------4------------
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("hello whats your name?");
+//
+//        String name = scanner.nextLine();
+//
+//        System.out.println("Hello "+name);
+
+//        -----------6---------
+
+//        String name = JOptionPane.showInputDialog("Enter your name");
+//        JOptionPane.showMessageDialog(null,"hello "+name);
+//
+//        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+//        JOptionPane.showMessageDialog(null,"your age is "+age+" "+name);
+
+//        -----------15-----------
+//        String[] cars = {"Camaro","corvette","tesla"};
+//
+//        cars[0]="mustang";
+//
+//        System.out.println(cars[0]);
+
+//        -------------19----------
+//        ArrayList<String> food = new ArrayList<String>();
+//        food.add("pizza");
+//        food.add("hamburger");
+//        food.add("hot dog");
+//
+//        System.out.println(food.get(1));
+
+//        ------------21-----------
+//        String[] animals = {"cat","dog","bird"};
+//
+//        for(String i :animals)
+//            System.out.println(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
